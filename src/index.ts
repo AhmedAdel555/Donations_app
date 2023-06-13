255244b8b9c1a2e9b55b214d66faaab0eea1fe21
AhmedAdel555 <ahmedbassiouni555@gmail.com>
2023-06-13 12:42:32 +0300

    add basic unit test by jasmine

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import express from "express"
+import express, {Application, Request, Response} from "express"
 
 const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+    return res.status(200).json({
+      message: "Hello World ❤"
+    })
+})
 
 app.listen(3000)
 
