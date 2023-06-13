c3022055168a4fbc76fa04d383f1c78cdceda3e6
AhmedAdel555 <ahmedbassiouni555@gmail.com>
2023-06-13 13:14:25 +0300

    add important middleware

--- src/__tests__/indexSpec.ts
+++ src/__tests__/indexSpec.ts
@@ -1,11 +1,11 @@
 import supertest from "supertest";
-import app from "../index"
+import app from "../index";
 
-const request = supertest(app)
+const request = supertest(app);
 
 describe("test the basic end point", () => {
   it("Get the / endpoint", async () => {
-      const respons = await request.get("/")
-      expect(respons.status).toBe(200)
-  })
-})
+      const respons = await request.get("/");
+      expect(respons.status).toBe(200);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
-import express, {Application, Request, Response} from "express"
+import express, {Application, Request, Response} from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import rateLimit from 'express-rate-limit';
 
-const app = express()
+// create the server
+const app = express();
+
+// json middleware
+app.use(express.json);
+// security middleware
+app.use(helmet());
+// log middleware
+app.use(morgan("common"));
+// rate limit requests
+app.use(rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
     return res.status(200).json({
       message: "Hello World ❤"
-    })
-})
+    });
+});
 
-app.listen(3000)
+// start the server
+app.listen(3000);
 
-export default app
+export default app ;
